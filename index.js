c8ab6ab1149eef5c67c831fe2ea55bfe54db3950
Konhin CM <maskmesta@gmail.com>
2021-07-02 12:24:15 -0500

    2

--- index.js
+++ index.js
@@ -1,46 +1,64 @@
 // Generación del canvas
 
 const myGameArea = {
-    canvas: document.createElement('canvas'),
-    frames: 0,
-    start: function () {
-        this.canvas.width = 480
-        this.canvas.height = 270
-        this
-        this.context = this.canvas.getContext('2d')
-        this.canvas.style.border = '1px solid green'
-        document.body.insertBefore(this.canvas, document.body.childNodes[0])
+  canvas: document.createElement('canvas'),
+  frames: 0,
+  start: function () {
+    this.canvas.width = 480
+    this.canvas.height = 270
+    this.context = this.canvas.getContext('2d')
+    this.canvas.style.border = '1px solid green'
+    document.body.insertBefore(this.canvas, document.body.childNodes[0])
 
-        this.interval = setInterval(updateGameArea, 20)
-    },
-    clear: function () {
-        this.context, clearRect(0, 0, this.canvas.width, this.canvas.height)
-    }
+    this.interval = setInterval(updateGameArea, 20)
+  },
+  clear: function () {
+    this.context.clearRect(0, 0, innerWidth, innerHeight)
+  }
 }
 
 class Component {
-    constructor(width, height, color, x, y) {
-        this.width = width
-        this.height = height
-        this.color = color
-        this.x = x
-        this.y = y
-        this.speedX = 0
-        this.speedY = 0
-    }
-    update() {
-        const ctx = myGameArea.context
-        ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.width, this.height)
-    }
+  constructor(width, height, color, x, y) {
+    this.speedX = 0
+    this.speedY = 0
+    this.width = width
+    this.height = height
+    this.color = color
+    this.x = x
+    this.y = y
+  }
+  update() {
+    const ctx = myGameArea.context
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+  }
+  left() {
+
+  }
+  right() {
+
+  }
+  top() {
+
+  }
+  button() {
+
+  }
+  newPos() {
+    this.x += this.speedX
+    this.y += this.speedY
+  }
+  crashWith() {
+
+  }
 }
 
 
 // Monitor de juego
 const updateGameArea = () => {
-    console.log("hola")
-    myGameArea.clear
-    player.update()
+  myGameArea.clear()
+  player.newPos()
+  player.update()
 }
 
 // Eventos
@@ -50,3 +68,27 @@ const player = new Component(30, 30, 'orange', 0, 110)
 
 
 myGameArea.start()
+
+document.addEventListener('keydown', (e) => {
+  switch (e.keyCode) {
+    case 37:
+      player.speedX -= 1
+      break
+    case 39:
+      player.speedX += 1
+      break
+    case 38:
+      player.speedY -= 1
+      break
+    case 40:
+      player.speedY += 1
+      break
+  }
+  console.log(player);
+})
+// Reset speed
+document.addEventListener('keyup', (event) => {
+  player.speedX = 0
+  player.speedY = 0
+  console.log('freno', player);
+})
